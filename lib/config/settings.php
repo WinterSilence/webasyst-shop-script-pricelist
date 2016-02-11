0e44b3fdc443a67182f66a14910d97c6fa29bd5b
Anton <WinterSilence@users.noreply.github.com>
2016-02-12 02:36:10 +0300

    Update settings.php

--- lib/config/settings.php
+++ lib/config/settings.php
@@ -1,9 +1,9 @@
 <?php
 return array(
     'cache_time' => array(
-        'value'        => '300',
+        'value'        => '3600',
         'title'        => /*_wp*/('Cache lifetime'),
-        'description'  => /*_wp*/('Is in seconds, the recommended value 600'),
+        'description'  => /*_wp*/('Is in seconds, the recommended value 3600'),
         'control_type' => waHtmlControl::INPUT,
     ),
 );
